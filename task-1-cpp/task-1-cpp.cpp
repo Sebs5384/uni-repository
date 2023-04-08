de93ccca2de983c350b75087a2276cf977d5abd6
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-04-08 20:28:41 +0200

    updates the solution
    changing the solution to take the first part of the guess the number logic

--- task-1-cpp/task-1-cpp.cpp
+++ task-1-cpp/task-1-cpp.cpp
@@ -1,20 +1,60 @@
-// task-1-cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+
+    int rangoMinimo, rangoMaximo, intentosMaximos, numeroAleatorio, adivinanza, i, intentosRestantes;
+    vector<int> numerosUsados;
+
+    cout << "Porfavor introduzca un rango minimo para el juego: " << endl;
+    cin >> rangoMinimo;
+    cout << "Ahora introduzca un rango maximo para el juego: " << endl;
+    cin >> rangoMaximo;
+    cout << "Cuantos intentos deseas tener en esta partida? " << endl;
+    cin >> intentosMaximos;
+
+    if (rangoMinimo > rangoMaximo) {
+        int $rangoMaximo = rangoMinimo;
+        rangoMinimo = rangoMaximo;
+        rangoMaximo = $rangoMaximo;
+    }
+
+    srand(time(NULL));
+    numeroAleatorio = rand() % (rangoMaximo - rangoMinimo + 1) + rangoMinimo;
+
+    for (i = 0; i < intentosMaximos; i++) {
+
+        intentosRestantes = (intentosMaximos - (i + 1));
+
+        cout << "Podes adivinar el numero aleatorio entre tus inputs? " << rangoMinimo << " y " << rangoMaximo << endl;
+        cin >> adivinanza;
+
+        if (adivinanza == numeroAleatorio) {
+            cout << "Adivinazte! el numero aleatorio era: " << numeroAleatorio << endl;
+            break;
+        }
+        else if (adivinanza < numeroAleatorio) {
+            cout << "Tu adivinanza es de menor valor a tu numero aleatorio, intenta de nuevo !" << endl;
+            cout << "Te quedan " << intentosRestantes << " intentos" << endl;
+            numerosUsados.push_back(adivinanza);
+        }
+        else if (adivinanza > numeroAleatorio) {
+            cout << "Tu adivinanza es de mayor valor a tu numero aleatorio, intenta de nuevo !" << endl;
+            cout << "Te quedan " << intentosRestantes << " intentos" << endl;
+            numerosUsados.push_back(adivinanza);
+        }
+
+
+
+    }
+
+    if (i == intentosMaximos) {
+        cout << "Se acabaron tus intentos, perdiste !" << endl;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
