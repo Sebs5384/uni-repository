2d9aa2978307a96f466652e50b51409fb80cb82d
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-04-08 22:53:49 +0200

    add guess printing
    adding a for loop that will print our user guess on every round

--- task-1-cpp/task-1-cpp.cpp
+++ task-1-cpp/task-1-cpp.cpp
@@ -7,13 +7,14 @@ using namespace std;
 
 int main() {
 
+    int i;
     int rangoMinimo;
     int rangoMaximo;
     int intentosMaximos;
     int numeroAleatorio;
     int adivinanza;
-    int i;
     int intentosRestantes;
+    bool adivinanzaRepetida;
     vector<int> numerosUsados;
 
     cout << "Porfavor introduzca un rango minimo para el juego: " << endl;
@@ -33,29 +34,41 @@ int main() {
     numeroAleatorio = rand() % (rangoMaximo - rangoMinimo + 1) + rangoMinimo;
 
     for (i = 0; i < intentosMaximos; i++) {
-
+        adivinanzaRepetida = false;
         intentosRestantes = (intentosMaximos - (i + 1));
 
         cout << "Podes adivinar el numero aleatorio entre tus inputs? " << rangoMinimo << " y " << rangoMaximo << endl;
         cin >> adivinanza;
 
-        if (adivinanza == numeroAleatorio) {
-            cout << "Adivinazte! el numero aleatorio era: " << numeroAleatorio << endl;
-            break;
-        }
-        else if (adivinanza < numeroAleatorio) {
-            cout << "Tu adivinanza es de menor valor a tu numero aleatorio, intenta de nuevo !" << endl;
-            cout << "Te quedan " << intentosRestantes << " intentos" << endl;
-            numerosUsados.push_back(adivinanza);
-        }
-        else if (adivinanza > numeroAleatorio) {
-            cout << "Tu adivinanza es de mayor valor a tu numero aleatorio, intenta de nuevo !" << endl;
-            cout << "Te quedan " << intentosRestantes << " intentos" << endl;
-            numerosUsados.push_back(adivinanza);
-        }
-        cout << "Los numeros que usastes son: " << endl;
         for (int j = 0; j < numerosUsados.size(); j++) {
-            cout << numerosUsados[j] << ", ";
+            if (numerosUsados[j] == adivinanza) {
+                i--;
+                cout << "Ya usaste este numero, intenta otro !" << endl;
+                adivinanzaRepetida = true;
+                break;
+            }
+        }
+           
+        if (!adivinanzaRepetida) {
+
+            if (adivinanza == numeroAleatorio) {
+                cout << "Adivinazte! el numero aleatorio era: " << numeroAleatorio << endl;
+                break;
+            }
+            else if (adivinanza < numeroAleatorio) {
+                cout << "Tu adivinanza es de menor valor a tu numero aleatorio, intenta de nuevo !" << endl;
+                cout << "Te quedan " << intentosRestantes << " intentos" << endl;
+                numerosUsados.push_back(adivinanza);
+            }
+            else if (adivinanza > numeroAleatorio) {
+                cout << "Tu adivinanza es de mayor valor a tu numero aleatorio, intenta de nuevo !" << endl;
+                cout << "Te quedan " << intentosRestantes << " intentos" << endl;
+                numerosUsados.push_back(adivinanza);
+            }
+        }
+        cout << "Los numeros que utilizaste hasta ahora son: ";
+        for (int k = 0; k < numerosUsados.size(); k++) {
+            cout << numerosUsados[k] << ", ";
         }
         cout << endl;
     }
