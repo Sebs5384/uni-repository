9fb418609db0c16c42297bd5c5bbab588d546c93
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-04-19 22:24:29 +0200

    updates the file
    adds the first excercise

--- task-1-cpp/fundamentos-programacion-exercises.cpp
+++ task-1-cpp/fundamentos-programacion-exercises.cpp
@@ -5,5 +5,27 @@ using namespace std
 
 int main() {
 
-	return 0
+        /*Escribir en lenguaje C un programa que calcule cuantos kilos de azúcar y yerba se
+        pueden comprar con determinado presupuesto.
+        1. Pida por teclado la cantidad de pesos que dispone.
+        2. Si la mitad de la cantidad de pesos se destina a comprar azúcar, una tercera
+        parte a comprar yerba y el resto no se gasta.Calcule cuántos kilos de azúcar y
+        yerba se pueden comprar con dicha cantidad de dinero, así como la cantidad
+        de dinero restante.
+        3. Muestre por pantalla los resultados.
+        Nota: El precio del kilo de azúcar es de $250 y el de yerba $850.
+        / Ejercicio 3
+        Escribir en lenguaje C un programa que calcule cuantos kilos de azúcar y yerba se
+        pueden comprar con determinado presupuesto.
+        1. Pida por teclado la cantidad de pesos que dispone.
+        2. Si la mitad de la cantidad de pesos se destina a comprar azúcar, una tercera
+        parte a comprar yerba y el resto no se gasta.Calcule cuántos kilos de azúcar y
+        yerba se pueden comprar con dicha cantidad de dinero, así como la cantidad
+        de dinero restante.
+        3. Muestre por pantalla los resultados.
+        Nota: El precio del kilo de azúcar es de $250 y el de yerba $850. */
+
+    return 0;
+
 }
+
