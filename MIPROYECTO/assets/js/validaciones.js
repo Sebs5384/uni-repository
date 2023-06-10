dfb7224b3e8570f5de166be350dee6a18ca49352
Sebs5384 <operationmadjackal@hotmail.com>
2023-06-10 15:38:30 -0300

    Agrega validacionId
    Esta funcion se va a encargar de validar el campo ID que el usuario usa para designar el ID de un producto

---
+++ MIPROYECTO/assets/js/validaciones.js
@@ -0,0 +1,18 @@
+document.querySelector("#guardar-operacion").onclick = function (event) {
+  const $campoId = document.querySelector("#id-categoria");
+  validarID($campoId);
+
+  event.preventDefault();
+};
+
+function validarID(campo) {
+  const $campo = convertirAString(campo.value);
+  if ($campo % 1 !== 0) {
+    return "El campo ID no acepta decimales";
+  } else if (!/^[1-9][0-9]*$/.test($campo)) {
+    return "El campo ID solo acepta numeros y no puede empezar con 0";
+  } else if (!/^.{1,4}$/.test($campo)) {
+    return "El campo ID solo acepta 4 digitos como maximo";
+  }
+  return "";
+}
