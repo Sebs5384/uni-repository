7909084be8b674e2082ead9868ccb652507c8c5b
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-06-11 00:54:22 -0600

    Agrega segunda validacion
    Esta es la segunda validacion que se encarga de validar el campo nombre-categorias

--- MIPROYECTO/assets/js/validaciones.js
+++ MIPROYECTO/assets/js/validaciones.js
@@ -5,14 +5,25 @@ document.querySelector("#guardar-operacion").onclick = function (event) {
   event.preventDefault();
 };
 
-function validarID(campo) {
-  const $campo = convertirAString(campo.value);
-  if ($campo % 1 !== 0) {
+function validarID(ID) {
+  const $id = convertirAString(ID.value);
+  if ($id % 1 !== 0) {
     return "El campo ID no acepta decimales";
-  } else if (!/^[1-9][0-9]*$/.test($campo)) {
+  } else if (!/^[1-9][0-9]*$/.test($id)) {
     return "El campo ID solo acepta numeros y no puede empezar con 0";
-  } else if (!/^.{1,4}$/.test($campo)) {
+  } else if (!/^.{1,4}$/.test($id)) {
     return "El campo ID solo acepta 4 digitos como maximo";
+  }
+  return "";
+}
+
+function validarCategorias(categoria) {
+  if (/\d/.test(categoria)) {
+    return "El nombre de la categoria no puede tener valores numericos";
+  } else if (/^$/.test(categoria)) {
+    return "El nombre de la categoria no debe estar vacio";
+  } else if (!/^[A-Z]+$/.test(categoria)) {
+    return "El nombre de la categoria debe ser solo en mayusculas";
   }
   return "";
 }
