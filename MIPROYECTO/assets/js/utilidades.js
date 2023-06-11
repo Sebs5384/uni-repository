70953a63f409bc95265880f37ae83efa89b03f34
Sebs5384 <operationmadjackal@hotmail.com>
2023-06-11 20:34:41 -0300

    Agrega manejarErrores
    Esta funcion se encarga de verificar si los inputs tienen errores, en el caso de tenerlos se le agrega un texto con el error en si.

--- MIPROYECTO/assets/js/utilidades.js
+++ MIPROYECTO/assets/js/utilidades.js
@@ -1,6 +1,26 @@
 const $botonGuardar = document.querySelector("#guardar-operacion");
+const $formulario = document.querySelector("#form-categoria");
 
 function convertirAString(integro) {
   const string = integro.toString();
   return string;
+}
+
+function manejarErrores(errores, selector) {
+  const llaves = Object.keys(errores);
+  const $error = document.querySelector(selector);
+  let erroresPresente = 0;
+
+  $error.innerText = "";
+  llaves.forEach((llave) => {
+    const error = errores[llave];
+    if (error) {
+      erroresPresente++;
+      $formulario[llave].className = "form-control";
+      $error.innerText = error;
+    } else {
+      $formulario[llave].className = "form-control";
+    }
+  });
+  return erroresPresente;
 }
