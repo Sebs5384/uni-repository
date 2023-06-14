1300946396f611e72d9a5c6c7cb47b9340b05d00
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-06-14 17:51:02 -0600

    Agrega el segundo test
    Esto testea el uso incorrecto de la pagina, agregando valores incorrectos y usando los botones para verificar si las clases y textos estan presentes

--- MIPROYECTO/cypress/e2e/categorias.cy.js
+++ MIPROYECTO/cypress/e2e/categorias.cy.js
@@ -22,4 +22,36 @@ describe("Testeo de alta categorias", () => {
     cy.get("#exitoso").should("have.class", "");
     cy.get("#exitoso").should("have.text", "");
   });
+
+  it("Testea el uso incorrecto de la pagina", () => {
+    cy.visit(URL);
+
+    cy.get("#input-id").type(12345);
+    cy.get("#input-categoria").type("placadevideo");
+
+    cy.get("#guardar-operacion").click();
+
+    cy.get("#input-id").should("have.class", "form-control is-invalid");
+    cy.get("#input-categoria").should("have.class", "form-control is-invalid");
+
+    cy.get("#error-id").should("have.text", "El campo ID solo acepta 4 digitos como maximo");
+    cy.get("#error-id").should("have.class", "invalid-feedback");
+    cy.get("#error-nombre").should("have.text", "El nombre de la categoria debe ser solo en mayusculas");
+    cy.get("#error-nombre").should("have.class", "invalid-feedback");
+
+    cy.get("#exitoso").should("have.class", "");
+    cy.get("#exitoso").should("have.text", "");
+
+    cy.get("#cancelar-operacion").click();
+
+    cy.get("#input-id").should("have.value", "");
+    cy.get("#input-categoria").should("have.text", "");
+    cy.get("#input-id").should("have.class", "form-control");
+    cy.get("#input-categoria").should("have.class", "form-control");
+
+    cy.get("#error-id").should("have.text", "");
+    cy.get("#error-id").should("have.class", "");
+    cy.get("#error-nombre").should("have.text", "");
+    cy.get("#error-nombre").should("have.class", "");
+  });
 });
