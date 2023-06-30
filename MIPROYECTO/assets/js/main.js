2be017be319fe253da6a669ab50afb8b5138239d
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-06-30 00:37:36 -0600

    Modifica main.js
    Ahora la definicion de enviarFormulario pasa a estar separada de inline a estar isolada y solo llamada cuando se le hace click botonEnviar

--- MIPROYECTO/assets/js/main.js
+++ MIPROYECTO/assets/js/main.js
@@ -1,8 +1,9 @@
 import { validarCampoId, validarCampoNombre } from "./validaciones.js";
 import { mostrarMensajeSiEsExitoso, resetearFormulario, $botonEnviar, $botonRestablecer, $campoId, $campoNombre } from "./ui.js";
 
-$botonEnviar.onclick = function enviarFormulario() {
-  mostrarMensajeSiEsExitoso(validarCampoId($campoId), validarCampoNombre($campoNombre));
-};
-
+$botonEnviar.onclick = enviarFormulario;
 $botonRestablecer.onclick = resetearFormulario;
+
+function enviarFormulario() {
+  mostrarMensajeSiEsExitoso(validarCampoId($campoId), validarCampoNombre($campoNombre));
+}
