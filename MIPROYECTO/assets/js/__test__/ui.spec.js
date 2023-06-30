149002b4890afdcd0f24cb6ae63a9e3a4ff8af7f
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-06-29 19:54:08 -0600

    Agrega los test resetearFormulario
    Esto agrega los test, happy path y edge cases de resetearFormulario

--- MIPROYECTO/assets/js/__test__/ui.spec.js
+++ MIPROYECTO/assets/js/__test__/ui.spec.js
@@ -1,4 +1,4 @@
-import { mostrarMensajeSiEsExitoso, mostrarErrores } from "../ui.js";
+import { mostrarMensajeSiEsExitoso, mostrarErrores, resetearFormulario } from "../ui.js";
 
 describe("Testea mostrarMensajeSiExitoso", () => {
   beforeEach(() => {
@@ -37,7 +37,6 @@ describe("Testea mostrarMensajeSiExitoso", () => {
       };
       const selector = "#error-id";
       const llaves = Object.keys(errores);
-      const $formulario = document.querySelector("#formulario");
       const $error = document.querySelector("#error-id");
       const result = mostrarErrores(llaves, errores, selector);
 
@@ -59,6 +58,78 @@ describe("Testea mostrarMensajeSiExitoso", () => {
       expect(result).toBe(0);
       expect($error.innerText).toBe("");
       expect($error.className).toBe("");
+    });
+  });
+
+  describe("Testea resetearFormulario", () => {
+    beforeEach(() => {
+      document.body.innerHTML = "<input type='number' id='input-id' name='id'><input type='text' id='input-nombre'><div id='error-id'><div id='mensaje-exitoso'><div id='error-nombre'></div></div></div>";
+    });
+
+    it("Deberia de resetear el formulario si este contiene clases exitosas", () => {
+      const $inputId = document.querySelector("#input-id");
+      const $inputNombre = document.querySelector("#input-nombre");
+      const $mensajeExito = document.querySelector("#mensaje-exitoso");
+
+      $inputId.value = "123";
+      $inputNombre.value = "PLACADEVIDEO";
+
+      $inputId.className = "form-control is-valid";
+      $inputNombre.className = "form-control is-valid";
+
+      $mensajeExito.innerText = "ID y Nombre guardado con exito";
+      $mensajeExito.className = "alert alert-success";
+
+      resetearFormulario();
+
+      expect($inputId.value).toBe("");
+      expect($inputId.className).toBe("");
+      expect($inputNombre.value).toBe("");
+      expect($inputNombre.className).toBe("");
+      expect($mensajeExito.innerText).toBe("");
+      expect($mensajeExito.className).toBe("");
+    });
+
+    it("Deberia de resetear el formulario si este contiene errores", () => {
+      const $inputId = document.querySelector("#input-id");
+      const $inputNombre = document.querySelector("#input-nombre");
+      const $errorId = document.querySelector("#error-id");
+      const $errorNombre = document.querySelector("#error-nombre");
+
+      $inputId.value = 1.2;
+      $inputNombre.value = "placadevideo";
+
+      $inputId.className = "form-control is-invalid";
+      $inputNombre.className = "form-control is-invalid";
+
+      $errorId.innerText = "El campo ID no acepta decimales";
+      $errorNombre.innerText = "El campo nombre solo acepta caracteres en mayuscula";
+      $errorId.className = "invalid-feedback";
+      $errorNombre.className = "invalid-feedback";
+
+      resetearFormulario();
+
+      expect($inputId.value).toBe("");
+      expect($inputId.className).toBe("");
+      expect($inputNombre.value).toBe("");
+      expect($inputNombre.className).toBe("");
+    });
+
+    it("Deberia de resetear el formulario si esta vacio", () => {
+      const $inputId = document.querySelector("#input-id");
+      const $inputNombre = document.querySelector("#input-nombre");
+
+      $inputId.value = "";
+      $inputNombre.value = "";
+      $inputId.className = "";
+      $inputNombre.className = "";
+
+      resetearFormulario();
+
+      expect($inputId.value).toBe("");
+      expect($inputId.className).toBe("");
+      expect($inputNombre.value).toBe("");
+      expect($inputNombre.className).toBe("");
     });
   });
 });
