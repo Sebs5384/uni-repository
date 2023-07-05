223d7dada5a9cff83d2393e243904b6f88aee7b6
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-07-05 05:41:51 -0600

    Actualiza servicios-db
    Esto crea la funcion que estara a cargar de crear las tablas correspondientes que el usuario ponga en el formulario principal.

--- MIPROYECTO/assets/js/servicios-db.js
+++ MIPROYECTO/assets/js/servicios-db.js
@@ -1,4 +1,4 @@
-import { actualizarMensajeFormulario } from "./ui.js";
+import { actualizarEstadoInput, actualizarMensajeFormulario } from "./ui.js";
 export async function insertarDatos() {
   try {
     const URL = "http://localhost/MIPROYECTO/class/categorias.php";
@@ -14,27 +14,51 @@ export async function insertarDatos() {
     console.log(json);
   } catch (error) {
     actualizarMensajeFormulario("error");
-    throw new Error("Error en promesa");
+    throw new Error("Error en promesa, no se pudo insertar los datos");
   }
 }
 
-export async function obtenerFilas() {
-  const URL = "http://localhost/MIPROYECTO/class/categorias.php";
-  const $respuesta = await fetch(URL, {
-    method: "GET",
-  });
-
-  const respuesta = await $respuesta.json();
-  const json = JSON.parse(respuesta);
-  return json;
+export async function obtenerFilas(table) {
+  try {
+    const URL = `http://localhost/MIPROYECTO/class/productos.php?table=${table}`;
+    const $respuesta = await fetch(URL, {
+      method: "GET",
+    });
+    const respuesta = await $respuesta.json();
+    return respuesta;
+  } catch (error) {
+    throw new Error("Error en promesa, no se pudo obtener las filas");
+  }
 }
 
-export async function obtenerTablas() {
-  const URL = "http://localhost/MIPROYECTO/class/productos.php";
-  const $respuesta = await fetch(URL, {
-    method: "GET",
-  });
-  const respuesta = await $respuesta.json();
-  const json = JSON.parse(respuesta);
-  return json;
+export async function obtenerCategorias() {
+  try {
+    const URL = "http://localhost/MIPROYECTO/class/categorias.php";
+    const $respuesta = await fetch(URL, {
+      method: "GET",
+    });
+
+    const respuesta = await $respuesta.json();
+    return respuesta;
+  } catch (error) {
+    throw new Error("Error en promesa, no se pudo obtener las tablas");
+  }
+}
+
+export async function crearCategoria() {
+  try {
+    const URL = "http://localhost/MIPROYECTO/class/categorias.php";
+    const $formulario = document.querySelector("#formulario");
+    const datosFormulario = new FormData($formulario);
+    const $respuesta = await fetch(URL, {
+      method: "POST",
+      body: datosFormulario,
+    });
+
+    return $respuesta.json();
+  } catch (error) {
+    actualizarEstadoInput("invalido", "#input-nombre");
+    actualizarMensajeFormulario("existente");
+    throw new Error("Error en promesa, no se pudo crear la categoria");
+  }
 }
