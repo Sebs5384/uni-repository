7346767d4304221abd90346dd03db708c38e9741
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-07-08 04:09:15 -0600

    Agrega la validaciones para productos
    Estas validaciones son para los inputs en productos HTML, los cuales seran dados a manejarErrores en forma de objeto

--- MIPROYECTO/assets/js/validaciones.js
+++ MIPROYECTO/assets/js/validaciones.js
@@ -1,44 +1,74 @@
 import { manejarErrores } from "./utilidades.js";
 
-function validarCamposProducto(producto) {
-  const $id = id.value;
-  if ($id % 1 !== 0) {
-    return "El campo ID no acepta decimales";
-  } else if (/^$/.test($id)) {
-    return "El campo ID no puede estar vacio";
-  } else if (!/^[1-9][0-9]*$/.test($id)) {
-    return "El campo ID solo acepta numeros y no puede empezar con 0";
-  } else if (!/^.{1,4}$/.test($id)) {
-    return "El campo ID solo acepta 4 digitos como maximo";
+function validarPrecio(precio) {
+  const $precio = precio.value;
+  if (/^$/.test($precio)) {
+    return "El campo precio no puede estar vacio";
+  } else if (!/^[1-9][0-9]*$/.test($precio)) {
+    return "El campo precio solo acepta numeros y no puede empezar con 0";
+  } else if (!/^.{1,8}$/.test($precio)) {
+    return "El campo precio solo acepta 8 digitos como maximo";
   }
   return "";
 }
 
 function validarNombre(nombre) {
-  const $categoria = nombre.value;
-  if (/\d/.test($categoria)) {
-    return "El campo nombre no puede tener valores numericos";
-  } else if (/^$/.test($categoria)) {
+  const $nombre = nombre.value;
+  if (/^$/.test($nombre)) {
     return "El campo nombre no debe estar vacio";
-  } else if (!/^[A-Z]+$/.test($categoria)) {
-    return "El campo nombre solo acepta caracteres en mayuscula";
+  } else if (!/^[A-Za-z0-9\s-]+$/.test($nombre)) {
+    return "El campo nombre solo acepta caracteres en mayuscula, minisculas, numeros y guiones";
+  } else if (!/^.{1,20}$/.test($nombre)) {
+    return "El campo nombre solo acepta 20 caracteres como maximo";
+  } else if (!/^.{5,}$/.test($nombre)) {
+    return "El campo nombre no puede tener menos de 5 caracteres";
   }
   return "";
 }
 
-export function validarCampoId($campoId) {
-  const errorEnCampoId = validarId($campoId);
+function validarDescripcion(descripcion) {
+  const $descripcion = descripcion.value;
+  if (!/^[A-Za-z0-9\s]+$/.test($descripcion)) {
+    return "El campo descripcion solo acepta caracteres en mayuscula, minisculas y numeros";
+  } else if (!/^.{1,50}$/.test($descripcion)) {
+    return "El campo descripcion solo acepta 50 caracteres como maximo";
+  }
+  return "";
+}
 
-  const errorEnCampo = {
-    id: errorEnCampoId,
+function validarCategoria(categoria) {
+  const $categoria = categoria.value;
+  if (/\d/.test($categoria)) {
+    return "El campo nombre no puede tener valores numericos";
+  } else if (/^$/.test($categoria)) {
+    return "El campo nombre no debe estar vacio";
+  } else if (!/^[A-Za-z\s]+$/.test($categoria)) {
+    return "El campo nombre solo acepta caracteres en mayuscula y minusculas";
+  } else if (!/^.{1,15}$/.test($categoria)) {
+    return "El campo categoria solo acepta 15 caracteres como maximo";
+  } else if (!/^.{5,}$/.test($categoria)) {
+    return "El campo categoria no puede tener menos de 5 caracteres";
+  }
+  return "";
+}
+
+export function validarCamposProducto($camposProducto) {
+  const errorEnCampoNombre = validarNombre($camposProducto);
+  const errorEnCampoDescripcion = validarDescripcion($camposProducto);
+  const errorEnCampoPrecio = validarPrecio($camposProducto);
+
+  const errorEnCampos = {
+    nombre: errorEnCampoNombre,
+    descripcion: errorEnCampoDescripcion,
+    precio: errorEnCampoPrecio,
   };
 
-  const esExistoso = manejarErrores(errorEnCampo, "#error-id") === 0;
+  const esExistoso = manejarErrores(errorEnCampos, ".productos") === 0;
   return esExistoso;
 }
 
 export function validarCampoCategoria($campoNombre) {
-  const errorEnCampoCategoria = validarNombre($campoNombre);
+  const errorEnCampoCategoria = validarCategoria($campoNombre);
 
   const errorEnCampo = {
     categoria: errorEnCampoCategoria,
