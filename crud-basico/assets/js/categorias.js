2598e5ef6379c4440cb42aaafd3f3f17ff7bf22e
Sebs5384 <operationmadjackal@hotmail.com>
2023-07-13 15:11:13 -0300

    Moves crud-basico
    This moves the basic crud to the main repository rather than inside the folder uni-repo

--- crud-basico/assets/js/categorias.js
+++
@@ -1,9 +0,0 @@
-import { enviarFormulario, resetearFormulario, $botonEnviar, $botonRestablecer } from "./ui.js";
-import { $campoCategoria } from "./ui.js";
-$botonEnviar.onclick = () => {
-  enviarFormulario("categoria", $campoCategoria);
-};
-
-$botonRestablecer.onclick = () => {
-  resetearFormulario("#input-categoria");
-};
--- crud-basico/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  verbose: true,
-  rootDir: "../MIPROYECTO/assets/js",
-  testEnvironment: "jsdom",
-  testPathIgnorePatterns: ["/node_modules/"],
-};
