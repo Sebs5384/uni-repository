3e79a15aa5bfc027452cb389eb7cd75de37b6435
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-07-13 15:03:23 -0600

    Actualiza productos

--- MIPROYECTO/assets/js/listado-productos.js
+++ MIPROYECTO/assets/js/listado-productos.js
@@ -1,6 +1,7 @@
 import { obtenerProductos } from "./servicios-db.js";
 import { crearTabla } from "./listado-categorias.js";
-import { $tabla } from "./ui.js";
+import { $tabla, mostrarMensajeSiEsExitoso } from "./ui.js";
+import { validarCamposProducto } from "./validaciones.js";
 
 export function verProductos(categoria) {
   localStorage.setItem("categoria", categoria);
@@ -19,17 +20,18 @@ async function cargarProductos() {
 
 async function manejarBotonesProductos(event) {
   const $botonElegido = event.target;
-  const $fila = $botonElegido.closest("tr");
-  const $nombreProducto = $fila.firstElementChild.innerText;
-  console.log($nombreProducto);
+  if ($botonElegido.classList.contains("actualizar")) {
+    const $camposEditar = document.querySelectorAll(`#editar${$botonElegido.id} .productos`);
+    console.log($camposEditar[0]);
+    console.log($camposEditar[1]);
+    console.log($camposEditar[2]);
+
+    mostrarMensajeSiEsExitoso(validarCamposProducto($camposEditar));
+  }
 }
 
 function listaProductos(productos, index) {
   const $lista = document.createElement("tr");
-
-  const $id = document.createElement("td");
-  $id.innerText = productos.id;
-  $id.className = "hidden";
 
   const $nombreProducto = document.createElement("td");
   const $descripcionProducto = document.createElement("td");
@@ -57,7 +59,6 @@ function listaProductos(productos, index) {
   $contenedorBotones.appendChild($espacioBlanco);
   $contenedorBotones.appendChild($botonBorrar);
 
-  $lista.appendChild($id);
   $lista.appendChild($nombreProducto);
   $lista.appendChild($descripcionProducto);
   $lista.appendChild($precio);
@@ -67,7 +68,7 @@ function listaProductos(productos, index) {
   return $lista;
 }
 
-function subListaProductos(index) {
+function subListaProductos(productos, index) {
   const $subFilaEditar = document.createElement("tr");
   $subFilaEditar.className = "collapsed-row";
 
@@ -80,31 +81,42 @@ function subListaProductos(index) {
 
   const $labelNombre = document.createElement("label");
   const $nuevoNombre = document.createElement("input");
+  const $errorNuevoNombre = document.createElement("strong");
   $labelNombre.className = "form-label small";
   $labelNombre.innerText = "Nuevo nombre";
   $nuevoNombre.type = "text";
-  $nuevoNombre.className = "form-control";
+  $nuevoNombre.className = `form-control productos`;
   $nuevoNombre.name = "nombre";
+  $errorNuevoNombre.id = "error-nombre";
+  $errorNuevoNombre.className = "";
 
   const $labelDescripcion = document.createElement("label");
   const $nuevaDescripcion = document.createElement("textarea");
+  const $errorNuevaDescripcion = document.createElement("strong");
   $labelDescripcion.className = "form-label small";
   $labelDescripcion.innerText = "Nueva descripcion";
-  $nuevaDescripcion.className = "form-control";
+  $nuevaDescripcion.className = `form-control productos`;
   $nuevaDescripcion.name = "descripcion";
+  $errorNuevaDescripcion.id = "error-descripcion";
+  $errorNuevaDescripcion.className = "";
 
   const $labelPrecio = document.createElement("label");
   const $nuevoPrecio = document.createElement("input");
+  const $errorNuevoPrecio = document.createElement("strong");
   $labelPrecio.className = "form-label small";
   $labelPrecio.innerText = "Nuevo precio";
   $nuevoPrecio.type = "number";
-  $nuevoPrecio.className = "form-control";
+  $nuevoPrecio.className = `form-control productos`;
   $nuevoPrecio.name = "precio";
+  $errorNuevoPrecio.id = "error-precio";
+  $errorNuevoPrecio.className = "";
 
   const $botonActualizar = document.createElement("button");
   const $botonCancelar = document.createElement("button");
   const $espacioBlanco = document.createTextNode(" ");
   $botonActualizar.innerText = "Actualizar";
+  $botonActualizar.id = productos.id;
+  $botonActualizar.name = productos.nombre_producto;
   $botonCancelar.innerText = "Cancelar";
   $botonActualizar.className = "btn btn-outline-secondary btn-sm actualizar";
   $botonCancelar.className = "btn btn-outline-secondary btn-sm";
@@ -115,24 +127,33 @@ function subListaProductos(index) {
   $contenedorNuevoNombre.className = "col-3";
   $contenedorNuevoNombre.appendChild($labelNombre);
   $contenedorNuevoNombre.appendChild($nuevoNombre);
+  $contenedorNuevoNombre.appendChild($errorNuevoNombre);
 
   const $contenedorNuevaDescripcion = document.createElement("td");
   $contenedorNuevaDescripcion.className = "col-4";
   $contenedorNuevaDescripcion.appendChild($labelDescripcion);
   $contenedorNuevaDescripcion.appendChild($nuevaDescripcion);
+  $contenedorNuevaDescripcion.appendChild($errorNuevaDescripcion);
 
   const $contenedorNuevoPrecio = document.createElement("td");
   $contenedorNuevoPrecio.className = "col-3";
   $contenedorNuevoPrecio.appendChild($labelPrecio);
   $contenedorNuevoPrecio.appendChild($nuevoPrecio);
+  $contenedorNuevoPrecio.appendChild($errorNuevoPrecio);
 
   const $contenedorBotones = document.createElement("td");
-  const $divBotones = document.createElement("div");
-  $divBotones.className = "container text-center";
-  $divBotones.appendChild($botonActualizar);
-  $divBotones.appendChild($espacioBlanco);
-  $divBotones.appendChild($botonCancelar);
-  $contenedorBotones.appendChild($divBotones);
+  const $botones = document.createElement("div");
+  $botones.className = "container text-center";
+  $botones.appendChild($botonActualizar);
+  $botones.appendChild($espacioBlanco);
+  $botones.appendChild($botonCancelar);
+  const $mensaje = document.createElement("strong");
+  const $divmensaje = document.createElement("div");
+  $mensaje.id = "mensaje-formulario";
+  $mensaje.innerText = "prueba";
+  $divmensaje.appendChild($mensaje);
+  $contenedorBotones.appendChild($botones);
+  $contenedorBotones.appendChild($divmensaje);
 
   $contenidoColapsable.appendChild($contenedorNuevoNombre);
   $contenidoColapsable.appendChild($contenedorNuevaDescripcion);
