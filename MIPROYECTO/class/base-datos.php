db8b0afec730ab95893de637a87281f68c770bf0
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-07-17 09:11:15 -0600

    Crea insert
    Esta funcion inserta nuevas filas a nuestra tablas

--- MIPROYECTO/class/base-datos.php
+++ MIPROYECTO/class/base-datos.php
@@ -45,6 +45,8 @@
 
         public function select($tabla, $filtro = null, $orden = null, $limite = null){
             try{
+                $conexion = $this->conexionBd->obtenerConexion();
+
                 $consulta = "SELECT * FROM $tabla";
                 if($filtro != null){
                     $consulta .= " WHERE " . $filtro;
@@ -55,8 +57,7 @@
                 if($limite != null){
                     $consulta .= " LIMIT " . $limite;
                 }
-
-                $conexion = $this->conexionBd->obtenerConexion();
+                
                 $consultaExitosa = $conexion->query($consulta);
 
                 if($consultaExitosa){
@@ -84,6 +85,36 @@
             }
 
         }
+
+        public function delete($tabla, $filtro = null){
+            try{
+                $conexion = $this->conexionBd->obtenerConexion();
+
+                $consulta = "DELETE FROM $tabla WHERE " . $filtro;
+                $consultaExitosa = $conexion->query($consulta);
+
+                if($consultaExitosa) echo "Eliminacion exitosa"; return true;
+        
+            } catch(Exception $error){
+                echo "Fallo la eliminacion: " . $error->getMessage();
+            }
+        }
+
+        public function insert($tabla, $columnas, $valores){
+            try{
+                $conexion = $this->conexionBd->obtenerConexion();
+
+                $consulta = "INSERT INTO $tabla ($columnas) VALUES ($valores)";
+                $consultaExitosa = $conexion->query($consulta);
+    
+                if($consultaExitosa) echo "Registro exitoso"; return true;
+    
+            } catch(Exception $error){
+                echo "Fallo la insercion: " . $error->getMessage();
+            }
+      
+
+        }
     }
 
     $db = new Conexion('mysql','localhost', 'root', '', 'miproyecto');
@@ -91,4 +122,6 @@
     $query = new Basedatos($db);
 
     $query->select('productos', null, 'ID asc', '3');
+    //$query->delete('productos', 'id = 2');
+    $query->insert('productos', 'nombre_producto, descripcion_producto, precio_producto, id_categoria', '"RTX-3000", "SERIES 3000", "3000", "1"');
 ?>
