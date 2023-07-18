f08069363d04297fb23f64cf6c93b3814cb53f36
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-07-18 02:17:01 -0600

    Crea Productos
    Esta clase se encarga de realizar las acciones correspondientes en el formulario de productos

---
+++ MIPROYECTO/class/productos.php
@@ -0,0 +1,55 @@
+<?php 
+
+    include "base-datos.php";
+
+    class Productos{
+
+        protected $id;
+        public $nombre;
+        public $descripcion;
+        public $precio;
+        public $categoria;
+        public $existe = false;
+
+
+        function __construct($id){
+            $conexion = new Conexion('mysql', 'localhost', 'root', '', 'miproyecto');
+            $conexion->conectar();
+            $query = new Basedatos($conexion);
+            $select = $query->select("productos", "id=?", null, null, array($id));
+            print_r($select);
+
+            if(isset($select[0]['ID'])){
+
+                $this->id = $select[0]['ID'];
+                $this->nombre = $select[0]['nombre_producto'];
+                $this->descripcion = $select[0]['descripcion_producto'];
+                $this->precio = $select[0]['precio_producto'];
+                $this->categoria = $select[0]['id_categoria'];
+                $this->existe = true;
+            }
+        }
+
+        public function mostrarObjeto(){
+            if($this->existe){
+                echo"<pre>";
+                print_r($this);
+                echo"</pre>";     
+            }
+           
+        }
+
+        
+        public static function listar(){
+            $conexion = new Conexion('mysql', 'localhost', 'root', '', 'miproyecto');
+            $conexion->conectar();
+            $query = new Basedatos($conexion);
+            return $query->select("productos");
+        }
+    }
+
+    $verProductos = new Productos(3);
+    $verProductos->mostrarObjeto();
+    $verProductos::listar();
+    
+?>
