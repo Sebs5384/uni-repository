a4fa5da17983339e37811faf4b89d9039f4dec00
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-07-20 07:04:15 -0600

    Crea el controlador de productos
    Aqui se controla que se envia y que se muestra dependiendo de lo que el usuario eliga en la lista.

---
+++ MIPROYECTO/backend/productos.php
@@ -0,0 +1,28 @@
+<?php 
+
+    include '../class/autoload.php';
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+        $nuevoProducto = new Productos(1);
+        $nuevoProducto->nombre=$_POST['nombre'];
+        $nuevoProducto->descripcion=$_POST['descripcion'];
+        $nuevoProducto->precio=$_POST['precio'];
+        $nuevoProducto->categoria=$_POST['categoria'];
+        $nuevoProducto->guardar();
+    
+    } else if(isset($_GET['agregar'])){
+        echo "<link href=\"../assets/css/estilos.css\" rel=\"stylesheet\" type=\"text/css\"/>";
+        echo "<style>.techbyte { display: none; }</style>";
+        echo "<img src=\"../assets/img/tech-byte-solutions.png\" class=\"techbyte-php\" />";
+
+        include 'views/productos.html';
+        die();
+    
+    }
+
+    $listarProductos = Productos::listar();
+    include './views/lista-productos.html'; 
+
+    
+?>
