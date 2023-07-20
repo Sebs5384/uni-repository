15d28d5a28b4e7291ac4bbf78ca2691a0b55c1ba
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-07-20 07:58:06 -0600

    Actualiza categorias.php
    Crea, mostrarObjetos, guardar, eliminar y insertar, estas tienen las mismas funcionalidades que las de productos.php y se van a usar en el controlador de categorias.php

---
+++ MIPROYECTO/class/categorias.php
@@ -0,0 +1,73 @@
+<?php 
+    include "base-datos.php";
+
+
+    class Categorias{
+        
+        protected $id;
+        public $nombre;
+        private $existe = false;
+
+        function __construct($id){
+            $conexion = new Conexion('mysql', 'localhost', 'root', '', 'miproyecto');
+            $conexion->conectar();
+            $query = new Basedatos($conexion);
+
+            $select = $query->select("categorias", "id=?", null, null, array($id));
+        
+            if(isset($select[0]['ID'])){
+                $this->id = $select[0]['ID'];
+                $this->nombre = $select[0]['nombre_categoria'];
+                $this->existe = true;
+            }
+        }
+        
+    
+    
+        public function mostrarObjeto(){
+            if($this->existe){
+                echo"<pre>";
+                print_r($this);
+                echo"</pre>";     
+            }     
+        }
+
+        public function guardar(){
+            if($this->existe){
+                print_r($this->existe);
+                return $this->actualizar();
+            } else{
+                return $this->insertar();
+            }
+        }
+        
+        public function eliminar(){
+            $conexion = new Conexion('mysql', 'localhost', 'root', '', 'miproyecto');
+            $conexion->conectar();
+            $query = new Basedatos($conexion);
+            $query->delete("categorias", "id=?", array($this->id));
+        }
+
+        private function insertar(){
+            $conexion = new Conexion('mysql', 'localhost', 'root', '', 'miproyecto');
+            $conexion->conectar();
+            $query = new Basedatos($conexion);
+            
+            $columnas = 'nombre_categoria';
+            $valores = '?';
+            $parametros = array($this->nombre);
+            $query->insert("categorias", $columnas, $valores, $parametros);
+            
+            if($query){
+                $this->id = $query;
+                $this->existe = true;
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    $xd = new Categorias(2);
+    $xd->mostrarObjeto();
+?>
