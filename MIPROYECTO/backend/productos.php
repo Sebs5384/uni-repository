6bd8c77dd0f1d2339560ceb76161bd76611358f3
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-07-24 18:48:22 -0600

    Actualiza productos.php
    Esto agrega la logica que se encarga de guardar las imagenes en la carpeta img

--- MIPROYECTO/backend/productos.php
+++ MIPROYECTO/backend/productos.php
@@ -9,9 +9,17 @@
         $nuevoProducto->descripcion=$_POST['descripcion'];
         $nuevoProducto->precio=$_POST['precio'];
         $nuevoProducto->categoria=$_POST['categoria'];
+        
+        $imagenName = $_FILES['imagen']['name'];
+        $imagenTmpName = $_FILES['imagen']['tmp_name'];
+        $folder = '../assets/img/';
+        move_uploaded_file($imagenTmpName, $folder.$imagenName);
+        $nuevoProducto->imagen=$folder.$imagenName;
+
         $nuevoProducto->guardar();
     
     } else if(isset($_GET['agregar'])){
+        
         echo "<link href=\"../assets/css/estilos.css\" rel=\"stylesheet\" type=\"text/css\"/>";
         echo "<style>.techbyte { display: none; }</style>";
         echo "<img src=\"../assets/img/tech-byte-solutions.png\" class=\"techbyte-php\" />";
