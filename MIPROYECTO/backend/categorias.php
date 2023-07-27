1e59cf8e88118a428d1bf93d0b8709dbb8e02315
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-07-27 01:28:59 -0600

    Actualiza categorias.php controlador
    Esto nos permite acceder al verdadero home y alta de productos desde el controlador.

--- MIPROYECTO/backend/categorias.php
+++ MIPROYECTO/backend/categorias.php
@@ -2,16 +2,26 @@
 
     include "../class/autoload.php";
 
+    $ocultarElementos = ".techbyte, #home, #formulario-productos { display: none; }";
+    $incluirEstilos = "href=\"../assets/css/estilos.css\" rel=\"stylesheet\" type=\"text/css\"";
+    $incluirImagen = "src=\"../assets/img/tech-byte-solutions.png\" class=\"techbyte-php\"";
+    $incluirHome = "id=\"nuevo-home\" href=\"../views/home.html\"";
+    $incluirAltaProductos = "id=\"formulario-productos-nuevo\" href=\"../views/productos.html\"";
+
     if($_SERVER['REQUEST_METHOD'] == "POST"){
 
         $nuevaCategoria = new Categorias(null);
         $nuevaCategoria->nombre=$_POST['nombre'];
         $nuevaCategoria->guardar();
 
-    } else if(isset($_GET['agregar'])){
-        echo "<link href=\"../assets/css/estilos.css\" rel=\"stylesheet\" type=\"text/css\"/>";
-        echo "<style>.techbyte { display: none; }</style>";
-        echo "<img src=\"../assets/img/tech-byte-solutions.png\" class=\"techbyte-php\" />";
+    } else if(isset($_GET['agregar-categoria'])){
+        echo <<<HTML
+        <style>{$ocultarElementos}</style>
+        <link {$incluirEstilos}/>
+        <img {$incluirImagen}/>
+        <a {$incluirHome}>Home</a>
+        <a {$incluirAltaProductos}>Alta de productos</a>
+        HTML;
         include "views/categorias.html";
         die();
     }
