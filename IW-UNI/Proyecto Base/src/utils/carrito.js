2b64fc1a9da6672e945622ed307103e01bf6182e
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-10-29 21:00:57 -0600

    Agrega localStorage
    Esto se encarga de almacenar y obtener el array desde localStorage

--- IW-UNI/Proyecto Base/src/utils/carrito.js
+++ IW-UNI/Proyecto Base/src/utils/carrito.js
@@ -3,14 +3,10 @@ const carritoProductos = [];
 const agregarAlCarrito = (productoId) => {
   if (productoId <= 0) return console.error('El id del producto no puede ser menor o igual a 0.');
 
-  const productoExistente = carritoProductos.find((producto) => producto.id === Number(productoId));
-  if (productoExistente) return alert('El producto ya existe en el carrito.');
-
   const producto = productos.find((producto) => producto.id === Number(productoId));
 
   if (producto !== undefined) {
     carritoProductos.push(producto);
-    console.table(carritoProductos);
   } else {
     console.error('El producto no existe.');
   }
@@ -18,6 +14,27 @@ const agregarAlCarrito = (productoId) => {
 
 function manejarClickProductos(event) {
   const productoClickeado = event.target.dataset.id;
+  const frutaClickeada = event.target.dataset.fruta;
 
-  agregarAlCarrito(productoClickeado);
+  try {
+    return obtenerProductosDeStorage('productos', frutaClickeada);
+  } catch (error) {
+    agregarAlCarrito(productoClickeado);
+    almacenarProducto('productos', carritoProductos);
+  }
+}
+
+function almacenarProducto(llave, valor) {
+  localStorage.setItem(llave, JSON.stringify(valor));
+}
+
+function obtenerProductosDeStorage(productos, fruta) {
+  const producto = JSON.parse(localStorage.getItem(productos));
+  const frutaAlmacendas = producto.some((producto) => producto.nombre === fruta);
+
+  if (!frutaAlmacendas) {
+    throw new Error('No se encontro el producto en memoria.');
+  }
+
+  return producto;
 }
